36dd33d762c82222a43954c9fb0ad3832d4ad577
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-04 09:19:47 -0500

    Initial commit

---
+++ src/main/java/com/challenge/backchallenge/BackChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.backchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackChallengeApplication.class, args);
+    }
+
+}
